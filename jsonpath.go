d5fb490d87235ed75fbe31dfc3f87d511885334f
Rogelio J. Baucells <rogelio@steelbridgelabs.com>
2023-03-27 17:56:30 -0400

    readme updates

--- jsonpath.go
+++ jsonpath.go
@@ -6,6 +6,8 @@
 
 package jsonpath
 
+// Gets evaluates the given JsonPath expression on the input data and returns the result.
+// The result is a single value if the JsonPath expression is definite, otherwise a list.
 func Get(data any, expression string, options ...Option) (any, error) {
 	// initial context
 	ctx := &pathContext{
@@ -51,6 +53,7 @@ func Get(data any, expression string, options ...Option) (any, error) {
 	return result, nil
 }
 
+// Sets evaluates the given JsonPath expression on the input data and sets the value to all matching paths.
 func Set(data any, expression string, value any, options ...Option) error {
 	// initial context
 	ctx := &pathContext{
